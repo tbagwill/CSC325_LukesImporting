5078b1663810c401f9edc4032e6cca9846a99d1b
Ty Bagwill <55044825+tbagwill@users.noreply.github.com>
2022-02-18 01:00:42 -0800

    Update lukeimporting.sql

--- lukeimporting.sql
+++ lukeimporting.sql
@@ -1,108 +1,101 @@
-#create database LukeImporting;
-#use LukeImporting; 
-
 /*
+create database LukeImporting;
+
+use LukeImporting; 
+
 create table Employee (
-	EmployeeID 			smallint unsigned auto_increment not null unique,
-    FirstName 			varchar(16) not null,
-    LastName 			varchar(24) not null,
-    DepartmentID 		tinyint unsigned not null,
+	EmployeeID 				smallint unsigned auto_increment not null unique,
+    FirstName 					varchar(16) not null,
+    LastName 					varchar(24) not null,
     primary key (EmployeeID)
-    );
-*/  
+    ); 
 
-#alter table Employee auto_increment = 10001;
+alter table Employee auto_increment = 10001;
 
-#insert into Employee(EmployeeID, FirstName, LastName, DepartmentID) 
-#values (null,  'Luke', 'Porter', 0), (null, 'Nathan', 'Adwater', 1), (null, 'Trenton', 'Dinko', 2);
+insert into Employee(EmployeeID, FirstName, LastName) 
+values (null,  'Luke', 'Porter'), (null, 'Nathan', 'Adwater'), (null, 'Trenton', 'Dinko'), (null, 'Darin', 'Sumfield');
 
-/*
-    create table ReceivingClerk (
-    fkEmployeeID		smallint unsigned not null,
-    fkDepartmentID		tinyint unsigned not null,
+create table ReceivingClerk (
+    fkClerkEmployeeID		smallint unsigned not null,
+    primary key (fkClerkEmployeeID),
+    foreign key (fkClerkEmployeeID) references Employee(EmployeeID)
+);
+
+create table PurchasingAgent (
+    fkEmployeeID				smallint unsigned not null,	
     primary key (fkEmployeeID),
     foreign key (fkEmployeeID) references Employee(EmployeeID)
-    );
-*/
-/*
-    create table PurchasingAgent (
-    fkEmployeeID		smallint unsigned not null,	
-    fkDepartmentID		tinyint unsigned not null,
-    primary key (fkEmployeeID),
-    foreign key (fkEmployeeID) references Employee(EmployeeID)
-    );
-*/
+);
 
 ###############################################################
 
-/*
-    create table Store (
-    StoreID 			smallint unsigned auto_increment not null unique,
-    StoreName 			varchar(32) not null,
+create table Store (
+    StoreID 						smallint unsigned auto_increment not null unique,
+    StoreName 					varchar(32) not null,
     primary key (StoreID)
-    );
-*/
+);
 
-#alter table Store auto_increment = 00500;
+alter table Store auto_increment = 00500;
 
-#insert into Store(StoreID, StoreName)
-#values (null, 'Jen and Berry\'s'), (null, 'Father Johnathon\'s'), (null, 'Big Al\'s Parts Place');
+insert into Store(StoreID, StoreName)
+values (null, 'Jen and Berry\'s'), (null, 'Father Johnathon\'s'), (null, 'Big Al\'s Parts Place');
 
-/*
-    create table ItemDescription (
-    ItemID				smallint unsigned not null auto_increment unique,
-    ItemName			varchar(24) not null unique,
-    Description			varchar(120),
-    primary key (ItemID)
-    );
-*/
-/*
-    create table PurchasedItem (
-    fkItemID			smallint unsigned,
-    fkPurchasingAgent	smallint unsigned,
-    InsuredValue		decimal(9, 2),
-    ArrivalCondition	varchar(16),
-    primary key (fkItemID),
-    foreign key (fkItemID) references ItemDescription(ItemID),
-    foreign key (fkPurchasingAgent) references PurchasingAgent(fkEmployeeID)
-    );
-*/
-/*
-    create table Purchase (
-    PurDateTime			datetime not null,
-    HowPurchased		varchar(20),
-    fkItemID			smallint unsigned,				
-    primary key (PurDateTime),
-    foreign key (fkItemID) references PurchasedItem(fkItemID)
-    );
-*/
-/*
-    create table Shipper (
-    ShipperID			smallint unsigned not null unique,	
+create table Shipper (
+    ShipperID				smallint unsigned auto_increment not null unique,	
     ShipperName			varchar(24),
     primary key (ShipperID)
-    );
+);
+
+insert into Shipper(ShipperID, ShipperName)
+values (null, 'UPS'), (null, 'Amazon'), (null, 'FedEx');
 */
+
 /*
-    create table Shipment (
-    ShipmentID			smallint unsigned not null unique,
-    fkShippedBy			smallint unsigned not null,
-    SentDateTime		datetime not null,
-    ReceivedDateTime	datetime not null,
-    fkReceivedBy		smallint unsigned,
+create table Shipment (
+    ShipmentID				mediumint(10) unsigned not null unique,
+    fkShipperID				smallint unsigned not null,
+    WhenSent				datetime,
+    WhenReceived		datetime,
     primary key (ShipmentID),
-    foreign key (fkShippedBy) references Shipper(ShipperID),
-    foreign key (fkReceivedBy) references ReceivingClerk(fkEmployeeID)
-    );
+    foreign key (fkShipperID) references Shipper(ShipperID)
+);
 */
 
+#insert into Shipment(ShipmentID, fkShipperID, WhenSent, WhenReceived)
+#values (0123456789, 00001, null, null), (9283476528, 00003, '2022-01-24 08:24:00', '2022-02-02 12:28:00'), (1728394232, 00002, '2022-02-14 10:17:00', null);
 /*
-insert into Employee(EmployeeID, FirstName, LastName, DepartmentID) 
-values (null,  'Luke', 'Porter', 0), (null, 'Nathan', 'Adwater', 1), (null, 'Trenton', 'Dinko', 2);
+create table ItemDescription (
+    ItemID							smallint unsigned not null auto_increment unique,
+    ItemName					varchar(24) not null unique,
+    ItemDescription			varchar(120),
+    primary key (ItemID)
+);
 
-insert into Store 
-values (null, 'Jen and Berry\'s'), (null, 'Father Johnathon\'s'), (null, 'Big Al\'s Parts Place');
+insert into ItemDescription(ItemID, ItemName, ItemDescription)
+values (null, 'TV Remote', 'Black Remote; Samsung TV; Model: 17.08.02'), (null, 'Projector Screen', null);
+
+
+
+create table Purchase (
+    PurDateTime			datetime not null default current_timestamp,
+    HowPurchased		varchar(20),
+    fkPurchasedBy		smallint unsigned,
+    primary key (PurDateTime),
+    foreign key (fkPurchasedBy) references PurchasingAgent(fkEmployeeID)
+);
+
+
+
+
+
+create table PurchasedItem (
+    fkWhenPurchased		datetime,
+    fkShipmentID				mediumint(10) unsigned,
+    InsuredValue				decimal(9, 2),
+    ArrivalCondition			varchar(16),
+    primary key (fkWhenPurchased),
+    foreign key (fkWhenPurchased) references Purchase(PurDateTime),
+    foreign key (fkShipmentID) references Shipment(ShipmentID)
+);
 */
 
-select *
-from store;
